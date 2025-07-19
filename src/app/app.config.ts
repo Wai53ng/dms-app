c94bf52864db6118c794839f3d4ce100a544cfa0
waiseng <waiseng.ng@outlook.com>
2025-07-19 11:55:16 +0800

    added ngx-translate as i18n into app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,27 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideTranslateService, TranslateLoader} from '@ngx-translate/core';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+    new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: httpLoaderFactory,
+            deps: [HttpClient],
+        },
+        useDefaultLang: true,
+        defaultLanguage: 'en',
+    }),
   ]
 };
