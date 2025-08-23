bb961e66435964c4ac24d35dc50eaac2c5d000dd
waiseng <waiseng.ng@outlook.com>
2025-08-23 15:07:19 +0800

    config i18n legacy for composition api

--- src/utils/language.ts
+++ src/utils/language.ts
@@ -2,6 +2,7 @@ import { createI18n } from 'vue-i18n'
 import { en_my } from '@/locales/en_my.ts'
 
 export const i18n = createI18n({
+    legacy: false,
     locale: 'en_my',
     fallbackLocale: 'en_my',
     messages: {
